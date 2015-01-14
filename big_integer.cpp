abe03ec1a0f2a0e2c1687ae68241081b04ff3421
Compte etudiant <etudiant@LGC.localdomain>
2015-01-14 13:56:06 +0100

    Ajout d'une fonction d'initialisation a 0

--- big_integer.cpp
+++ big_integer.cpp
@@ -15,16 +15,23 @@ void test_add(char* valeur1, int taille1, char* valeur2, int taille2, char* resu
 	}
 }
 
+void initialiser_biginteger(char * valeur, int taille) {
+	for (int i = 0; i < taille; ++i)	{
+		valeur[i] = '0';
+	}
+}
+
 int main(int argc, char *argv[]) {
 
 	char* valeur1 = "8794565467498434654216542546541321564624132165432324";
 	int taille1 = strlen(valeur1);
 	char* valeur2 = "9876546746542657865165461321654613212156432132164212";
 	int taille2 = strlen(valeur2);
-	char* resultat = (char*)malloc((taille1+1)*sizeof(char));
-	for (int i = 0; i < taille1+1; ++i)	{
-		resultat[i] = '0';
-	}
+	int tailleResultat = taille1+1;
+	char* resultat = (char*)malloc((tailleResultat)*sizeof(char));
+
+	initialiser_biginteger(resultat,tailleResultat);
+	cout << resultat << endl;
 	test_add(valeur1,taille1,valeur2,taille2,resultat);
 	cout << " " << valeur1 << endl;
 	cout << " " << valeur2 << endl;
