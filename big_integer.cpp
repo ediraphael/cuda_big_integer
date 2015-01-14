0a72950def22fff1db9dd336171416e2ab62dbcb
Compte etudiant <etudiant@LGC.localdomain>
2015-01-14 13:52:36 +0100

    Changement d'implémentation de début pour partir sur des char *

--- big_integer.cpp
+++ big_integer.cpp
@@ -4,8 +4,8 @@
 #include <stdlib.h> 
 using namespace std;
 
-void test_add(char* valeur1, char* valeur2, char* resultat) {
-	for (int i = strlen(resultat)-1; i >= 1; --i) {
+void test_add(char* valeur1, int taille1, char* valeur2, int taille2, char* resultat) {
+	for (int i = taille1; i >0 ; --i) {
 		int nouveau  = valeur1[i-1] - '0' + valeur2[i-1] - '0' + resultat[i] - '0';
 		if( nouveau > 9 ) {
 			resultat[i-1] += 1;
@@ -17,29 +17,19 @@ void test_add(char* valeur1, char* valeur2, char* resultat) {
 
 int main(int argc, char *argv[]) {
 
-	string toto1 = "87945654674984346542165425465413215646241321654321324";
-		   toto1 = "99999999999999999999999999999999999999999999999999999";
-	string toto2 = "98765467465426578651654613216546132121564321321654212";
-	cout << "Big integer: " << endl;
-	cout << "taille toto1 :"<< toto1.length() << endl;
-	char test1[toto1.length()];
-	char test2[toto2.length()];
-	char test3[toto2.length()+1];
-	cout << "taille test1 :" << sizeof(test1) << endl;
-	cout << "premiere element :" << toto1[0] << endl;
-	for (int i = 0; i < sizeof(test1); ++i) {
-		test1[i] = toto1[i];
-		test2[i] = toto2[i];
+	char* valeur1 = "8794565467498434654216542546541321564624132165432324";
+	int taille1 = strlen(valeur1);
+	char* valeur2 = "9876546746542657865165461321654613212156432132164212";
+	int taille2 = strlen(valeur2);
+	char* resultat = (char*)malloc((taille1+1)*sizeof(char));
+	for (int i = 0; i < taille1+1; ++i)	{
+		resultat[i] = '0';
 	}
-	for (int i = 0; i < sizeof(test3); ++i) {
-		test3[i] = '0';
-	}
-	
+	test_add(valeur1,taille1,valeur2,taille2,resultat);
+	cout << " " << valeur1 << endl;
+	cout << " " << valeur2 << endl;
+	cout << resultat << endl;
 
-	
-	test_add(test1,test2,test3);
-	cout << "0" << toto1 << endl;
-	cout << "0" << toto2 << endl;
-	cout << test1 << endl;
+
 	return 0;
 }
