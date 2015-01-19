555f662d72d6b0d550bce8539a8eb4859baf4caa
Compte etudiant <etudiant@LGC.localdomain>
2015-01-19 17:12:27 +0100

    Mise en place d'une structure pour représenter les bigInteger

--- big_integer.cpp
+++ big_integer.cpp
@@ -4,6 +4,12 @@
 #include <stdlib.h> 
 using namespace std;
 
+struct BigInteger {
+	char* valeur;
+	int taille;
+	bool est_positif;
+};
+
 void test_add(char* valeur1, int taille1, char* valeur2, int taille2, char* resultat) {
 	for (int i = taille1; i >0 ; --i) {
 		int nouveau  = valeur1[i-1] - '0' + valeur2[i-1] - '0' + resultat[i] - '0';
@@ -23,19 +29,25 @@ void initialiser_biginteger(char * valeur, int taille) {
 
 int main(int argc, char *argv[]) {
 
-	char* valeur1 = "8794565467498434654216542546541321564624132165432324";
-	int taille1 = strlen(valeur1);
-	char* valeur2 = "9876546746542657865165461321654613212156432132164212";
-	int taille2 = strlen(valeur2);
-	int tailleResultat = taille1+1;
-	char* resultat = (char*)malloc((tailleResultat)*sizeof(char));
-
-	initialiser_biginteger(resultat,tailleResultat);
-	cout << resultat << endl;
-	test_add(valeur1,taille1,valeur2,taille2,resultat);
-	cout << " " << valeur1 << endl;
-	cout << " " << valeur2 << endl;
-	cout << resultat << endl;
+	BigInteger bigInteger1;
+	bigInteger1.valeur = "8794565467498434654216542546541321564624132165432324";
+	bigInteger1.taille = strlen(bigInteger1.valeur);
+	bigInteger1.est_positif = true;
+	BigInteger bigInteger2;
+	bigInteger2.valeur = "9876546746542657865165461321654613212156432132164212";
+	bigInteger2.taille = strlen(bigInteger1.valeur);
+	bigInteger2.est_positif = true;
+	BigInteger resultat;
+	resultat.taille = bigInteger1.taille+1;
+	resultat.valeur = (char*)malloc((resultat.taille)*sizeof(char));
+	resultat.est_positif = true;
+	initialiser_biginteger(resultat.valeur,resultat.taille);
+	
+	cout << resultat.valeur << endl;
+	test_add(bigInteger1.valeur,bigInteger1.taille,bigInteger2.valeur,bigInteger2.taille,resultat.valeur);
+	cout << " " << bigInteger1.valeur << endl;
+	cout << " " << bigInteger2.valeur << endl;
+	cout << resultat.valeur << endl;
 
 
 	return 0;
